6d3cababe9861cf8bbaa01979bf62b8455d59fd5
Will1016wang <52725879+Will1016wang@users.noreply.github.com>
2020-06-02 17:21:26 -0400

    Add files via upload

--- Planner.m
+++ Planner.m
@@ -1,6 +1,6 @@
 %% Planner
-initialLocation = [1,1; 8,1; 1,8; 8,8]; %representing the initial location of each robot
-goalLocation = [3,1; 5,2; 6,3; 2,2];% teh position of tasks
+initialLocation = [-10,1; 8,1; 4,8; 8,8]; %representing the initial location of each robot
+goalLocation = [15,1; 2,2; 19,9; 17,6];% teh position of tasks
 M = zeros(4,4);
 for i = 1:4
     for j = 1:4
@@ -9,34 +9,119 @@ for i = 1:4
 end
 [A,C]= munkres(M); % A is the optimal assignment matrix, C is the total minimum cost
 pi = A.*M; % calculate pi*
-%% Formation of trajectories
+%% plots of trajectories
 % This step is to form four linear trajectories for the four robots, which has already been well calculated above.  Because of the feature of HUngarian Algorithm, the four trajectories should not intersect each other.
 
 %In MATLAB document, waypointTrajectory can help us build a trajectory objectory, howecer, now I just focus on the primitive linea lines.
 %labels = cellstr(num2str([1:4]')) % create the order and lables
-
+a = zeros(1,4);
+b = zeros(1,4);
 for i = 1:4
     for j = 1:4
         if A(i,j) == 1
             figure(1)
-            axis([-5,10,-5,10])
+            axis([-20,20,-20,20])
             plot([initialLocation(i,1), goalLocation(j,1)], [initialLocation(i,2),goalLocation(j,2)])
+            coefficients = polyfit([initialLocation(i,1),goalLocation(j,1)],[initialLocation(i,2),goalLocation(j,2)],1);
+            a(i) = coefficients(1);
+            b(i) = coefficients(2);
             OrderOfRobot = num2str(i);
             OrderOfGoal = num2str(j);
-            text(initialLocation(i,1), initialLocation(i,2),OrderOfRobot,'FontSize',10)
+            text(initialLocation(i,1), initialLocation(i,2),OrderOfRobot,'FontSize',12)
             text(initialLocation(i,1), initialLocation(i,2),'o','color','g')
-            text(goalLocation(j,1), goalLocation(j,2),OrderOfGoal,'FontSize',12)
+            text(goalLocation(j,1), goalLocation(j,2),OrderOfGoal,'FontSize',15)
             text(goalLocation(j,1), goalLocation(j,2),'*','color','r')
-            xlim([0,10])
-            ylim([0,10])
-            hold on            
+            axis manual
+            hold on
         end
     end
 end
-
 % The primitive tradjectory is done, the next step is to construct the collision avoidance function into planner, then refine tradjectory.
 
+% Problem: when the shortest oath is verticle, the line parameters of line function fails to represent it.
+
 % In this stage, I do not want too many things about differential cars, that may left to the next few steps.
+%% functions of trajectories
+
+% In this section, i will plot those trajectories in a new form for further utilization.
+yTrajectory = cell(1,4);
+xTrajectory = cell(1,4);
+
+for i = 1:4
+    for j = 1:4
+        if A(i,j) == 1
+            xTrajectory{i} = linspace(initialLocation(i,1),goalLocation(j,1),100);
+            yTrajectory{i} = a(i) * xTrajectory{i} + b(i);
+        end
+    end
+end
+%% robot animination
+
+% In this section, the robot will be modelled as a point and move along the trajectory
+
+% Velocity havn't been sepcified, and waypointTrajectory havn't been used.
+
+% In this program, there must be four object moving along four trajectory.
+
+ax = gca; % gca: Get handle to the current axis. Return the handle to the current axis in the current figure.
+h1 = hgtransform('Parent', ax);
+h2 = hgtransform('Parent', ax);
+h3 = hgtransform('Parent', ax);
+h4 = hgtransform('Parent', ax);
+hold on
+%for i = 1:length(xTrajectory)    
+    plot(xTrajectory{1}(1),yTrajectory{1}(1),'ro','Parent',h1); % In this line, I deleted some parameters such as 'VerticalAlignment', 'top' and so on, i may should add them on later.
+    plot(xTrajectory{2}(1),yTrajectory{2}(1),'ro','Parent',h2);
+    plot(xTrajectory{3}(1),yTrajectory{3}(1),'ro','Parent',h3);
+    plot(xTrajectory{4}(1),yTrajectory{4}(1),'ro','Parent',h4);
+    hold off
+    t1 = text(xTrajectory{1}(1),yTrajectory{1}(1),num2str(yTrajectory{1}(1)),'Parent',h1, 'VerticalAlignment','top','FontSize',14);
+    t2 = text(xTrajectory{2}(1),yTrajectory{2}(1),num2str(yTrajectory{2}(1)),'Parent',h2, 'VerticalAlignment','top','FontSize',14);
+    t3 = text(xTrajectory{3}(1),yTrajectory{3}(1),num2str(yTrajectory{3}(1)),'Parent',h3, 'VerticalAlignment','top','FontSize',14);
+    t4 = text(xTrajectory{4}(1),yTrajectory{4}(1),num2str(yTrajectory{4}(1)),'Parent',h4, 'VerticalAlignment','top','FontSize',14);    
+    for k = 2:100 % 99 iterations
+        m1 = makehgtform('translate', xTrajectory{1}(k)-xTrajectory{1}(1),yTrajectory{1}(k) - yTrajectory{1}(1), 0);
+        m2 = makehgtform('translate', xTrajectory{2}(k)-xTrajectory{2}(1),yTrajectory{2}(k) - yTrajectory{2}(1), 0);
+        m3 = makehgtform('translate', xTrajectory{3}(k)-xTrajectory{3}(1),yTrajectory{3}(k) - yTrajectory{3}(1), 0);
+        m4 = makehgtform('translate', xTrajectory{4}(k)-xTrajectory{4}(1),yTrajectory{4}(k) - yTrajectory{4}(1), 0);
+        h1.Matrix = m1;
+        h2.Matrix = m2;
+        h3.Matrix = m3;
+        h4.Matrix = m4;
+        t1.String = num2str(xTrajectory{1}(k)) + "," + num2str(yTrajectory{1}(k));
+        t2.String = num2str(xTrajectory{2}(k)) + "," + num2str(yTrajectory{2}(k));
+        t3.String = num2str(xTrajectory{3}(k)) + "," + num2str(yTrajectory{3}(k));
+        t4.String = num2str(xTrajectory{4}(k)) + "," + num2str(yTrajectory{4}(k));
+        drawnow
+    end
+    hold off
+    % Yeah, I know this program is very tedius and looks as a rigmorale. I will fix it when i finish all the essential procedures.
+    
+%end
+
+% So, now the question is, how to let four robots(points) moving somutaneously on the four trajjectories?
+%%  Clearance Function theta
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 %% Hungarian Algorithm
 function [assignment,cost] = munkres(costMat)
@@ -182,16 +267,3 @@ end
 assignment(validRow,validCol) = starZ(1:nRows,1:nCols);
 cost = sum(costMat(assignment));
 end
-
-
-
-
-    
-    
-    
-    
-    
-
-
-
-
